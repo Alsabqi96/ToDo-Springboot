812fb3b021d472293e30a30d2bb6152f74215d1e
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-06 20:50:56 +0400

    Added Setter and Getter to CreateTodoRequest

--- src/main/java/com/codearchitects/todoapp/RequestObjects/CreateTodoRequest.java
+++ src/main/java/com/codearchitects/todoapp/RequestObjects/CreateTodoRequest.java
@@ -1,4 +1,26 @@
 package com.codearchitects.todoapp.RequestObjects;
 
 public class CreateTodoRequest {
+
+    private String title;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String description;
+
 }
+
