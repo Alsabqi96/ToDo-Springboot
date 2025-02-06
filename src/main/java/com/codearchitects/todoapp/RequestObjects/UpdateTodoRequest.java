4e39f426279fdbfb62b904da06edfcd0746e086c
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-06 21:10:10 +0400

    Added Setter and Getter to UpdateTodoRequest

--- src/main/java/com/codearchitects/todoapp/RequestObjects/UpdateTodoRequest.java
+++ src/main/java/com/codearchitects/todoapp/RequestObjects/UpdateTodoRequest.java
@@ -1,4 +1,32 @@
 package com.codearchitects.todoapp.RequestObjects;
 
 public class UpdateTodoRequest {
+    private String title;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    private String description;
+    private boolean completed;
 }
