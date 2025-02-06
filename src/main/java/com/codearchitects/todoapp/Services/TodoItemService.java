80717d78c7e1a0ab72adf6de707dd9cc22950cd0
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-07 00:34:01 +0400

    Adding code that Converts an entity into a DTO to control the data exposed via API responses.

--- src/main/java/com/codearchitects/todoapp/Services/TodoItemService.java
+++ src/main/java/com/codearchitects/todoapp/Services/TodoItemService.java
@@ -10,6 +10,16 @@ import org.springframework.stereotype.Service;
 public class TodoItemService {
     @Autowired
     private TodoItemRepository repository;
+
+    private TodoItemDTO convertToDTO(TodoItem todoItem) {
+        TodoItemDTO dto = new TodoItemDTO();
+        dto.setId(todoItem.getId());
+        dto.setTitle(todoItem.getTitle());
+        dto.setDescription(todoItem.getDescription());
+        dto.setCompleted(todoItem.getCompleted());
+        return dto;
+    }
+
 }
 
 
