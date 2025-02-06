0cca75b2903dc41a1e2efa6718ac31981a214a6f
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-06 20:34:51 +0400

    Added TodoItemDTO for API response structuring

--- src/main/java/com/codearchitects/todoapp/DTOs/TodoItemDTO.java
+++ src/main/java/com/codearchitects/todoapp/DTOs/TodoItemDTO.java
@@ -1,4 +1,43 @@
 package com.codearchitects.todoapp.DTOs;
 
+
 public class TodoItemDTO {
-}
+        private Long id;
+        private String title;
+        private String description;
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private boolean completed;
+
+    }
