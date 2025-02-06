72cc30ea917a113ccf87f69cb9c1a1ceec9a972c
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-06 21:21:28 +0400

    Added TodoItemRepository for database

--- src/main/java/com/codearchitects/todoapp/Repositories/TodoItemRepository.java
+++ src/main/java/com/codearchitects/todoapp/Repositories/TodoItemRepository.java
@@ -1,4 +1,8 @@
 package com.codearchitects.todoapp.Repositories;
 
-public class TodoItemRepository {
-}
+import com.codearchitects.todoapp.Models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+    public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {}
+
