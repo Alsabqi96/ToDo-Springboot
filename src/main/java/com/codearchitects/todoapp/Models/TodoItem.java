0b019a2b1f3dd22ab8dd01948c287317bdee6ad3
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-07 15:38:10 +0400

    i have fixed some code and arangement setter and getter

--- src/main/java/com/codearchitects/todoapp/Models/TodoItem.java
+++ src/main/java/com/codearchitects/todoapp/Models/TodoItem.java
@@ -1,57 +1,14 @@
 package com.codearchitects.todoapp.Models;
 
-
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 
 @Entity
 public class TodoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String title;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(Boolean completed) {
-        this.completed = completed;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
     private String description;
     private Boolean completed;
     private LocalDateTime createdAt;
@@ -61,9 +18,15 @@ public class TodoItem {
         createdAt = LocalDateTime.now();
     }
 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Boolean getCompleted() { return completed; }
+    public void setCompleted(Boolean completed) { this.completed = completed; }
 }
-
-
-
