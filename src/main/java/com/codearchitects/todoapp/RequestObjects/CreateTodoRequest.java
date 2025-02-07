02ea7b0a0fcf9cb9dcf9fb9cbefb499376c7f17f
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-07 15:40:50 +0400

    i have fixed some code and arangement setter and getter

--- src/main/java/com/codearchitects/todoapp/RequestObjects/CreateTodoRequest.java
+++ src/main/java/com/codearchitects/todoapp/RequestObjects/CreateTodoRequest.java
@@ -1,26 +1,12 @@
 package com.codearchitects.todoapp.RequestObjects;
 
 public class CreateTodoRequest {
-
     private String title;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     private String description;
 
-}
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
