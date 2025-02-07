4fa0f2d02a5d5b9d5679ab2e975c4d24dcf3244c
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-07 15:58:01 +0400

    Updated some codes and adding repository notation

--- src/main/java/com/codearchitects/todoapp/Repositories/TodoItemRepository.java
+++ src/main/java/com/codearchitects/todoapp/Repositories/TodoItemRepository.java
@@ -2,7 +2,11 @@ package com.codearchitects.todoapp.Repositories;
 
 import com.codearchitects.todoapp.Models.TodoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-    public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {}
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+
+}
 
