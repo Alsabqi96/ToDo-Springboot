e5544d22a81d8354a41daf0c98c5e0246d8ccaaa
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-07 15:41:50 +0400

    i have fixed some code and arangement setter and getter

--- src/main/java/com/codearchitects/todoapp/RequestObjects/UpdateTodoRequest.java
+++ src/main/java/com/codearchitects/todoapp/RequestObjects/UpdateTodoRequest.java
@@ -2,35 +2,15 @@ package com.codearchitects.todoapp.RequestObjects;
 
 public class UpdateTodoRequest {
     private String title;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
     private String description;
-    private boolean completed;
+    private Boolean completed;
 
-    public Boolean getCompleted() {
-        return completed;
-    }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Boolean getCompleted() { return completed; }
+    public void setCompleted(Boolean completed) { this.completed = completed; }
 }
