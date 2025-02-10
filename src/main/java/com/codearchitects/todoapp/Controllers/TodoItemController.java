f6df338cff0be87c52d9028799afe2eed02aa130
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-10 19:17:37 +0400

    Added CrossOrigin Controller

--- src/main/java/com/codearchitects/todoapp/Controllers/TodoItemController.java
+++ src/main/java/com/codearchitects/todoapp/Controllers/TodoItemController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/todos")
+@CrossOrigin(origins = "*")
 public class TodoItemController {
     @Autowired
     private TodoItemService service;
