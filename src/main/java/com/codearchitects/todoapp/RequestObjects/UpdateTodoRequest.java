d6411a6f37c0ab1a0e1864c6d0caa62631611fbf
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-12 19:17:09 +0400

    Added some commits and rearrange setters and getters

--- src/main/java/com/codearchitects/todoapp/RequestObjects/UpdateTodoRequest.java
+++ src/main/java/com/codearchitects/todoapp/RequestObjects/UpdateTodoRequest.java
@@ -7,12 +7,30 @@ public class UpdateTodoRequest {
     private String description;
     private Boolean completed;
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    //setters and getters
+    public String getTitle() {
+        return title;
+    }
 
-    public Boolean getCompleted() { return completed; }
-    public void setCompleted(Boolean completed) { this.completed = completed; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
 }
