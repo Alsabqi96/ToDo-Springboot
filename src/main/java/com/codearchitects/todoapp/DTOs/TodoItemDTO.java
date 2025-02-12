4a62f0c8f32ac1b36445b77e86a08aab64ff00a4
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-12 18:32:42 +0400

    Added some commits

--- src/main/java/com/codearchitects/todoapp/DTOs/TodoItemDTO.java
+++ src/main/java/com/codearchitects/todoapp/DTOs/TodoItemDTO.java
@@ -6,6 +6,7 @@ public class TodoItemDTO {
     private String description;
     private Boolean completed;
 
+    // Setter and Getter
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
