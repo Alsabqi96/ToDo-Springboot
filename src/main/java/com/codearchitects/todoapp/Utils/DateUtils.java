1797b08ef9bd1436d59c8cf4a544aa8beedfde05
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-12 19:25:36 +0400

    Fixed code and added some  commit

--- src/main/java/com/codearchitects/todoapp/Utils/DateUtils.java
+++ src/main/java/com/codearchitects/todoapp/Utils/DateUtils.java
@@ -4,9 +4,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 
+//utility class for formatting date and time values
 public class DateUtils {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
+    //Formats a LocalDateTime object into a string.
     public static String formatDate(LocalDateTime dateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return dateTime.format(formatter);
+        return dateTime.format(FORMATTER);
     }
 }
