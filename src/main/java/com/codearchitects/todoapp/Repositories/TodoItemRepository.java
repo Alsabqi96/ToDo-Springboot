d321976cc3e0e4d9daa311aeefd628bbc8763001
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-12 19:09:25 +0400

    Added some commits

--- src/main/java/com/codearchitects/todoapp/Repositories/TodoItemRepository.java
+++ src/main/java/com/codearchitects/todoapp/Repositories/TodoItemRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
+//repository interface fot todo item
+//extends jparepository to provide built in CRUD methods
 @Repository
 public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
 
 }
-
