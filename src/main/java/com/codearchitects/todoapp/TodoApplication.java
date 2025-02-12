3d92378ac92df87a6f91c1d79a26fbc57fc7b381
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-12 19:28:17 +0400

    added some commit

--- src/main/java/com/codearchitects/todoapp/TodoApplication.java
+++ src/main/java/com/codearchitects/todoapp/TodoApplication.java
@@ -3,12 +3,11 @@ package com.codearchitects.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//entry point for the spring boot todo application
 @SpringBootApplication
-public class TodoappApplication {
-
+public class TodoApplication {
 	public static void main(String[] args) {
-
+		SpringApplication.run(TodoApplication.class, args);
 	}
-
 }
 
