b984e9e05c032f72ad0b36eb8d50b6d1ebb88df8
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-18 08:50:37 +0400

    Added another commits to enhance clarity

--- src/main/java/com/codearchitects/todoapp/Controllers/TodoItemController.java
+++ src/main/java/com/codearchitects/todoapp/Controllers/TodoItemController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RequestMapping(path = "/api/v1/todos")
 public class TodoItemController {
 
+    /*Logger instance for logging errors and info*/
     private static final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
 
     @Autowired
