1d45b2deb5ecd36c344282252255d1bc6f6af11c
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-23 23:16:51 +0400

    Minor bug fixes

---
+++ src/main/java/com/codearchitects/todoapp/Config/UserRepository.java
@@ -0,0 +1,26 @@
+package com.codearchitects.todoapp.Config;
+
+
+import com.codearchitects.todoapp.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u From User u WHERE u.userName=:un")
+    User findByUserName(@Param("un") String userName);
+
+    Boolean existsByEmail(String email);
+}
+
+
+
+
+
+
+
+
+
