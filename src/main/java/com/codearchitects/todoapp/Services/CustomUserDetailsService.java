d56c3056b056e4cf668ef6968970a38acfef830f
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-24 19:27:18 +0400

    Fixed bug for username not found issue

---
+++ src/main/java/com/codearchitects/todoapp/Services/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.codearchitects.todoapp.Services;
+
+import com.codearchitects.todoapp.Models.User;
+import com.codearchitects.todoapp.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if(user==null){
+            throw  new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUserName())
+                .password(user.getPassword())
+                .roles(user.getRole().name()) // Ensure role handling
+                .build();
+    }
+}
+
+
+
+
+
+
+
