db863539036c882280fe944a45bb860813baa859
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-02-24 09:29:51 +0400

    Added custom exception handling

---
+++ src/main/java/com/codearchitects/todoapp/Exceptions/InvalidCredentialsException.java
@@ -0,0 +1,12 @@
+package com.codearchitects.todoapp.Exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
+
+
+
+
+
