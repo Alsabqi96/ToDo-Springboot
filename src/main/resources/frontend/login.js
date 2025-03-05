59e642cf6ade5c2d6d0b34e09ca26ed83fddf8a6
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-03-05 10:42:02 +0400

    Updated js

---
+++ src/main/resources/frontend/login.js
@@ -0,0 +1,26 @@
+document.getElementById("loginForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const userName = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("http://localhost:8080/auth/signin", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ userName, password })
+        });
+
+        if (!response.ok) throw new Error("Invalid credentials");
+
+        const result = await response.json();
+        localStorage.setItem("token", result.token);
+        window.location.href = "index.html";
+
+    } catch (error) {
+        document.getElementById("error-message").textContent = error.message;
+    }
+});
+
+
+
