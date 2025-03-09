a14aa3e56d7b6f84e01d2c8429072a3422b4a3f6
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-03-09 08:54:56 +0400

    Updated code

--- src/main/java/com/codearchitects/todoapp/Exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/codearchitects/todoapp/Exceptions/UserAlreadyExistsException.java
@@ -1,13 +1,16 @@
 package com.codearchitects.todoapp.Exceptions;
 
 public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message){
+
+    public UserAlreadyExistsException(String message) {
         super(message);
     }
+
     public UserAlreadyExistsException(String message, Throwable cause) {
         super(message, cause);
     }
 }
+
 
 
 
