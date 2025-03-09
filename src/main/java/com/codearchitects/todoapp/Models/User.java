8794b7cbcee2768721a471b926279b1dfb01ca2f
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2025-03-09 08:59:06 +0400

    Updated code

--- src/main/java/com/codearchitects/todoapp/Models/User.java
+++ src/main/java/com/codearchitects/todoapp/Models/User.java
@@ -1,30 +1,139 @@
 package com.codearchitects.todoapp.Models;
 
+
 import com.codearchitects.todoapp.Enums.Role;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
 
 @Entity
-public class User extends BaseEntity {
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // Auto-generate the ID
+    private Long id; // Primary key
 
     @NotBlank(message = "Email can not be empty")
     @Column(nullable = false, unique = true)
-    public String email;
+    private String email;
 
     @NotBlank(message = "Password can not be empty")
     @Column(nullable = false)
-    public String password;
+    private String password;
 
     @NotBlank(message = "Username can not be empty")
     @Column(nullable = false, unique = true)
-    public String userName;
+    private String userName;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    public Role role;
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Email can not be empty") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email can not be empty") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Password can not be empty") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank(message = "Password can not be empty") String password) {
+        this.password = password;
+    }
+
+    public @NotBlank(message = "Username can not be empty") String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(@NotBlank(message = "Username can not be empty") String userName) {
+        this.userName = userName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    /*// Private constructor for the builder
+    private User(UserBuilder builder) {
+        this.id = builder.id;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.userName = builder.userName;
+        this.role = builder.role;
+    }
+
+    // Default constructor (needed for JPA)
+    public User() {
+    }
+
+    // Builder class
+    public static class UserBuilder {
+        private Long id;
+        private String email;
+        private String password;
+        private String userName;
+        private Role role;
+
+        public UserBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public UserBuilder setRole(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+    // Static method to get a new builder instance
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
@@ -56,8 +165,5 @@ public class User extends BaseEntity {
 
     public void setRole(Role role) {
         this.role = role;
-    }
+    }*/
 }
-
-
-
